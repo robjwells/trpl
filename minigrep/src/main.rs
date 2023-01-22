621106136c6e602036abae8bb6fc51e0a30f1511
Rob Wells <rob@robjwells.com>
2023-01-22 11:37:30 +0000

    12 - minigrep - 12.04 implement search

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,10 +8,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!(
-        "Searching for {:?}\nIn file {:?}",
-        config.query, config.file_path
-    );
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
