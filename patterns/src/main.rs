08b841ce7f05203e2aa9084d3ec7932dcb9b20d1
Rob Wells <rob@robjwells.com>
2023-02-14 11:54:01 +0000

    18 - Patterns

---
+++ patterns/src/main.rs
@@ -0,0 +1,42 @@
+#![allow(clippy::vec_init_then_push)]
+
+fn main() {
+    let favourite_colour: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(colour) = favourite_colour {
+        println!("Using your favourite color, {colour}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is a green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background colour");
+        } else {
+            println!("Using orange as the background colour");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    let point = (1, 2);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({:?}, {:?})", x, y);
+}
